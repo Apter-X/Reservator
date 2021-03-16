2881e24bb1e5ec9e20c51a03d5a8951eb09e387d
Iliass Raihani <i.raihani@outlook.com>
2021-03-16 22:03:36 +0100

    remvove reservation view

--- Reservator/Startup.cs
+++ Reservator/Startup.cs
@@ -54,6 +54,9 @@ namespace Reservator
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "AdminPanel",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
