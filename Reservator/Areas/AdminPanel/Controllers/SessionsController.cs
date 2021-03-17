c2d269361005a4c31ba1c643c75ea47ceb282078
Iliass Raihani <i.raihani@outlook.com>
2021-03-17 16:53:18 +0100

    crud session with entity

---
+++ Reservator/Areas/AdminPanel/Controllers/SessionsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Reservator.Data;
+using Reservator.Models;
+
+namespace Reservator.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class SessionsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SessionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: AdminPanel/Sessions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Sessions.ToListAsync());
+        }
+
+        // GET: AdminPanel/Sessions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .FirstOrDefaultAsync(m => m.SessionID == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        // GET: AdminPanel/Sessions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminPanel/Sessions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SessionID,DateID")] Session session)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(session);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(session);
+        }
+
+        // GET: AdminPanel/Sessions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            return View(session);
+        }
+
+        // POST: AdminPanel/Sessions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SessionID,DateID")] Session session)
+        {
+            if (id != session.SessionID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(session);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SessionExists(session.SessionID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(session);
+        }
+
+        // GET: AdminPanel/Sessions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Sessions
+                .FirstOrDefaultAsync(m => m.SessionID == id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return View(session);
+        }
+
+        // POST: AdminPanel/Sessions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var session = await _context.Sessions.FindAsync(id);
+            _context.Sessions.Remove(session);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SessionExists(int id)
+        {
+            return _context.Sessions.Any(e => e.SessionID == id);
+        }
+    }
+}
