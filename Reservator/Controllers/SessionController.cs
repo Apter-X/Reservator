2eae6ed0172f5c87e232591fd200c75e12b75ce2
Iliass Raihani <i.raihani@outlook.com>
2021-03-18 22:14:53 +0100

    add creation new session if not exist functionality

--- Reservator/Controllers/SessionController.cs
+++ Reservator/Controllers/SessionController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Reservator.Data;
+using Reservator.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,20 +24,29 @@ namespace Reservator.Controllers
         }
 
         // GET: AdminPanel/Sessions/Details/5
-        public async Task<IActionResult> Details(string? date)
+        public async Task<IActionResult> Details(string date)
         {
+            if (!SessionExists(date))
+            {
+                var s = new Session
+                {
+                    DateID = date
+                };
+
+                _db.Add(s);
+                await _db.SaveChangesAsync();
+            }
+
             var session = await _db.Sessions
                 .Include(a => a.Reservations)
                 .FirstOrDefaultAsync(m => m.DateID == date);
 
-            if (session == null)
-            {
-                return Index();
-            }
-
             return View(session);
         }
+
+        private bool SessionExists(string date)
+        {
+            return _db.Sessions.Any(e => e.DateID == date);
+        }
     }
-
-
 }
