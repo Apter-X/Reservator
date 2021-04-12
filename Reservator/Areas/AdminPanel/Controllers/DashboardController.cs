1ba906e8302aac5236238a94ea96f1507dc99a94
Iliass Raihani <i.raihani@outlook.com>
2021-04-12 10:06:33 +0000

    add get jeson method

--- Reservator/Areas/AdminPanel/Controllers/DashboardController.cs
+++ Reservator/Areas/AdminPanel/Controllers/DashboardController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Reservator.Data;
 using System;
 using System.Collections.Generic;
@@ -27,6 +28,21 @@ namespace Reservator.Areas.AdminPanel.Controllers
             ViewData["Refused"] = _context.Reservations.Where(c => c.Statement == "Refused").Count();
 
             return View();
+        }
+
+        public string GetJson()
+        {
+            Dictionary<string, int> statements = new Dictionary<string, int>
+            {
+                { "InProgress", _context.Reservations.Where(c => c.Statement == "InProgress").Count() },
+                { "Canceled", _context.Reservations.Where(c => c.Statement == "Canceled").Count() },
+                { "Confirmed", _context.Reservations.Where(c => c.Statement == "Confirmed").Count() },
+                { "Refused", _context.Reservations.Where(c => c.Statement == "Refused").Count() }
+            };
+
+            string json = JsonConvert.SerializeObject(statements, Formatting.Indented);
+
+            return json;
         }
     }
 }
