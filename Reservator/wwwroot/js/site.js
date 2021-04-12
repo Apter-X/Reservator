dfa13a12341a4d18dc34cf71dd697191bc1467bc
Iliass Raihani <i.raihani@outlook.com>
2021-04-12 10:14:44 +0000

    Update site.js

--- Reservator/wwwroot/js/site.js
+++ Reservator/wwwroot/js/site.js
@@ -1,9 +1,6 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
-var gmail = document.getElementById("gmail");
-addEventListener()
 // Call the dataTables jQuery plugin
 $(document).ready(function () {
     $('#dataTable').DataTable();
